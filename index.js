4216ccae8e512f567b70dad596fdd35ec82d1d98
Ian Muisyo Ngila <ngilaian@gmail.com>
2022-06-18 12:54:01 +0300

    Done.

--- index.js
+++ index.js
@@ -1,7 +1,14 @@
 function isPalindrome(word) {
-  // Write your algorithm here
-}
+  
+  for (let i = 0; i < word.length / 2; i++) {
+   
+    const j = word.length - 1 - i;
+   
+    if (word[i] !== word[j]) return false;
+  }
 
+  return true;
+}
 /* 
   Add your pseudocode here
 */
@@ -23,3 +30,4 @@ if (require.main === module) {
 }
 
 module.exports = isPalindrome;
+
