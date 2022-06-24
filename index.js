a2b91fad9b9e633df36a8d5b10b36f93160d2674
Ian Muisyo Ngila <ngilaian@gmail.com>
2022-06-24 09:30:50 +0300

    Done.

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-function isPalindrome(word) {
-  
-  for (let i = 0; i < word.length / 2; i++) {
-   
-    const j = word.length - 1 - i;
-   
-    if (word[i] !== word[j]) return false;
-  }
+function reverse(word) {
+  return word.split("").reverse().join("");
+}
 
-  return true;
+function isPalindrome(word) {
+  // Write your algorithm here
+  const reversedWord = reverse(word);
+
+  return word === reversedWord;
 }
 /* 
   Add your pseudocode here
